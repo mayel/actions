8e342120ff42917f19f92a6fd55b45e000eb1968
Mayel de Borniol <115318+mayel@users.noreply.github.com>
2021-04-15 19:32:32 +0200

    Update entrypoint.sh

--- bin/ghr/entrypoint.sh
+++ bin/ghr/entrypoint.sh
@@ -4,7 +4,7 @@ set -ex
 gh_user=`echo $GITHUB_REPOSITORY | sed -e 's/\/.*//g'`
 gh_repo=`echo $GITHUB_REPOSITORY | sed -e 's/.*\///g'`
 
-version=`grep 'version:' mix.exs | cut -d '"' -f2`
+version=v`grep 'version:' mix.exs | cut -d '"' -f2`
 
 ghr -v
 sh -c "ghr -t $GITHUB_TOKEN -u $gh_user -r $gh_repo -n $version $version $ASSETS"
